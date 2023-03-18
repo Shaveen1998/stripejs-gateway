b98e87be2ebcf81667e6762a9004df2529f2bbed
iamshaveenvf <shaveenfernando1@gmail.com>
2023-03-18 15:07:37 +0530

    added a json to store items

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config()
+}
+
+const StripeSecretKey = process.env.STRIPE_SECRET_KEY
+const StripePublicKey = process.env.STRIPE_PUBLIC_KEY
+console.log(StripeSecretKey, StripePublicKey)
+
 const express = require('express');
+const fs = require('fs')
 
 const app = express();
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
+
+app.get('/', function(req,res){
+    res.render('index.ejs')
+})
 
 app.listen(3000)
     
